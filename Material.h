ebfff82ee198f062024bcc0f4a504c4c09ddbf0c
Mariaxyz <quijiam@gmail.com>
2020-06-11 22:31:50 -0500

    Material.h

---
+++ Material.h
@@ -0,0 +1,23 @@
+
+# ifndef MATERIAL_H_
+# define  MATERIAL_H
+# include  < iostream > _
+# include  < string.h >
+using std :: string;
+clase  Material {
+	privado:
+		int * codigo;
+	string autor;
+	string titulo;
+	int anio;
+	bool estado;
+	
+public:
+	Material();
+	Material(int codigo[5], string autor, string titulo, string anio, string estado)
+		int getCodigo();
+	string getAutor();
+	string getTitulo();
+	int getAnio();
+	bool getEstado();
+};
