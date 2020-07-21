12303ac97c5a3b763dd89bb3b273b941e2bb99d1
henry-uriel-bustinza <66810830+henry-uriel-bustinza@users.noreply.github.com>
2020-07-21 18:04:36 -0500

    avance de prestamo
    Avance de la clase de prestamo corigiendo errores

---
+++ Prestamo.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+class Prestamo{
+public:
+	Prestamo();
+	void altas(int,int,int,int,int,int,int,int,int,int);
+	void consultas();
+	void actualizar();
+	void buscar_id();
+};
